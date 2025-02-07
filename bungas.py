f10ae70dfae6b8f1b35be92e000ed2f84e7df129
TantoFaz65517 <jaziel.campelo@escolar.ifrn.edu.br>
2025-02-07 10:36:00 -0300

    bungas 3

--- bungas.py
+++ bungas.py
@@ -22,10 +22,7 @@ def geras_carte(mod):
                     elif x == 1:
                         linha.append(random.randint(11, 20))
                     else:
-                        linha.append(random.randint(21, 30))
-                        print(linha)
-    else:
-        print("escolha um modo válido!(rapido/demorado)")        
+                        linha.append(random.randint(21, 30))   
 
 #a = ("demorado")
 #geras_carte(a)
@@ -43,19 +40,25 @@ def bingus():
     
     carte_n = 2 if modo == "rapido" else 4
     for x in range (carte_n):
-        nome = input(f"Nome do jogador {i + 1}: ")
+        nome = input(f"Nome do jogador {x + 1}: ")
         play.append(nome)
         carte.append(geras_carte(modo))
-    sorteado = []
+    sort = []
     game_end = False
     cartelas_fechadas = set()
+    while not game_end:
+        dezena = random.randid(1, 40)
+        while dezena in sort:
+            dezena = random.randid(1, 40)
+        sort.append(dezena)
+        carte_ex(carte, play, sort)
+        for y, carte in enumerate(cart)
+
+bingus()
         
         
-        
-        
-        
-    while not jogo_terminado:
-        dezena = random.randint(1, 40)
-        while dezena in sorteados:
-            dezena = random.randint(1, 40)
-        sorteados.append(dezena)
+#while not jogo_terminado:
+#    dezena = random.randint(1, 40)
+#    while dezena in sorteados:
+#        dezena = random.randint(1, 40)
+#    sorteados.append(dezena)
